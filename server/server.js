ac594090a7bbe8c9730f63309293e94d363ef28d
tivrama <tivrama@yahoo.com>
2016-07-04 10:27:21 -0700

    (Server/Tests) Expanding tests, server, and routes

--- server/server.js
+++ server/server.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-// var methodOverride = require('method-override'); //Can add to package.json if needed
+var methodOverride = require('method-override'); //Can add to package.json if needed
 var mongoose = require('mongoose');
 
 // For setting up Routes (Uncomment when routes are complete)
-// var middleware = require('./config/middleware.js'); 
+var middleware = require('./config/middleware.js'); 
 
 // configuration ===========================================
  
 //Set up Middleware and Routes (Uncomment when routes are complete)
-// middleware(app, express);
+middleware(app, express);
 
 // db files
 var db = require('./db/db');
@@ -44,17 +44,17 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-// app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /client/img will be /img for users
-app.use(express.static(__dirname + '/client')); 
+app.use(express.static(__dirname + '../client')); 
 //====For Posible Later Use===============
 
 
 
 
-// Configure routes 
-require('./food/foodRoutes')(app);
+// Configure routes // Currently being required in config/middleware.js
+// require('./food/foodRoutes')(app);
 // require('./user/userRoutes')(app);
 
 
