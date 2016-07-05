9a465c60a9d388007de8c1e0aca9fec02e7966a4
tivrama <tivrama@yahoo.com>
2016-07-05 08:57:32 -0700

    (server) fine tuning server

--- server/food/foodController.js
+++ server/food/foodController.js
@@ -1,19 +1,38 @@
-//this contains and modifies food
+// grab the food model
+// var Food = require('./foodModel.js'); //Uncomment once foodModel is ready to export
+
+//this adds and modifies food
 module.exports = {
     
-  //invrementor
+  // ===Testing & Debugging============
   foodFightTest: 'FoodFight!!',
+  objectIdCounter: 1,
+  messages: [
+    {
+      text: 'hello world'
+    }
+  ],
+  // ===Testing & Debugging============
 
 
-  //adds yes vote and checks for result
-  addFood: function () {
+  //adds a food item to the db
+  addFood: function (food) {
     //add food to db
     console.log('testing add food');
+    return 'testing return in addFood';
   },
 
+  //gets all food from db
   getAllFood: function () {
     //get all food from db
     console.log('showing all food');
+    var food = [{hungry: 'eat'}, {full: 'stop eating'}];
+    return food;
+  },
+
+  //gets all food from a subcatagory in db
+  getFoodSubCatagory: function () {
+    console.log('showing all food in a subcatagory');
   }
 
 };
--- server/server.js
+++ server/server.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
-// var morgan = require('morgan');
-// var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 
 var middleware = require('./config/middleware.js'); 
 
 
-
 // configuration ===========================================
  
-//Set up Middleware and Routes (Uncomment when routes are complete)
+//Set up Middleware and Routes
 middleware(app, express);
 
 // db files
@@ -21,43 +17,11 @@ var db = require('./db/db');
 var port = process.env.PORT || 3000; 
 
 
-
 //------!!----------------------
 // connect to mongoDB database 
 // (uncomment after establishing credentials in db/db.js)
 mongoose.connect(db.url); 
 //------!!----------------------
-
-
-
-
-//====For Posible Later Use===============
-// // used for logging incoming request
-// app.use(morgan('dev'));
-
-// // parse application/json 
-// app.use(bodyParser.json()); 
-
-// // parse application/vnd.api+json as json
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
-
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true })); 
-
-// // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-// app.use(methodOverride('X-HTTP-Method-Override')); 
-
-// // set the static files location /client/img will be /img for users
-// app.use(express.static(__dirname + '../client')); 
-//====For Posible Later Use===============
-
-
-
-
-// Configure routes // Currently being required in config/middleware.js
-// require('./food/foodRoutes')(app);
-// require('./user/userRoutes')(app);
-
 
 
 // startup our app at http://localhost:3000
