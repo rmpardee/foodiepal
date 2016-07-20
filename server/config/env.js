59db537878c3cb211bb26f04a770b4a63821886b
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-07-19 17:06:32 -0700

    (deploy) Adds all env variables to env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,3 +1,8 @@
 module.exports = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || require('./config.js').expJwt.scrt,
+  mlab: {
+    dbuser: process.env.MLAB_DBUSER || require('./config.js').mlab.dbuser,
+    dbpassword: process.env.MLAB_DBPASSWORD || require('./config.js').mlab.dbpassword
+  }
 };
