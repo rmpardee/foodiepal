1194ecfbda2b30d4bfb3eb919c601c86354b97af
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-07-19 17:28:38 -0700

    (deploy) Adds deploy specific version for env variables

--- server/config/env.js
+++ server/config/env.js
@@ -1,8 +1,19 @@
+// DEPLOYED version:
 module.exports = {
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || require('./config.js').expJwt.scrt,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
   mlab: {
-    dbuser: process.env.MLAB_DBUSER || require('./config.js').mlab.dbuser,
-    dbpassword: process.env.MLAB_DBPASSWORD || require('./config.js').mlab.dbpassword
+    dbuser: process.env.MLAB_DBUSER,
+    dbpassword: process.env.MLAB_DBPASSWORD
   }
 };
+
+// LOCAL version:
+// module.exports = {
+//   port: 3000,
+//   jwtSecret: require('./config.js').expJwt.scrt,
+//   mlab: {
+//     dbuser: require('./config.js').mlab.dbuser,
+//     dbpassword: require('./config.js').mlab.dbpassword
+//   }
+// };
