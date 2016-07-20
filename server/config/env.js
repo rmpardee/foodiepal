d736eff054833a3b6b11a4ea90e110186daf909e
tivrama <tivrama@yahoo.com>
2016-07-20 16:20:03 -0700

    (authentication) fixing emails

--- server/config/env.js
+++ server/config/env.js
@@ -1,23 +1,19 @@
 // DEPLOYED version:
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET,
-  mlab: {
-    dbuser: process.env.MLAB_DBUSER,
-    dbpassword: process.env.MLAB_DBPASSWORD
-  },
-  postmark: process.env.POSTMARK
-};
-
-// LOCAL version:
 // module.exports = {
 //   jwtSecret: process.env.JWT_SECRET,
 //   mlab: {
-//     dbuser: require('./config.js').mlab.dbuser,
-//     dbpassword: require('./config.js').mlab.dbpassword
-//   },
-//   postmark: require('./config.js').postmark
-// };
 //     dbuser: process.env.MLAB_DBUSER,
 //     dbpassword: process.env.MLAB_DBPASSWORD
-//   }
+//   },
+//   postmark: process.env.POSTMARK
 // };
+
+// LOCAL version:
+module.exports = {
+  jwtSecret: require('./config.js').expJwt.scrt,
+  mlab: {
+    dbuser: require('./config.js').mlab.dbuser,
+    dbpassword: require('./config.js').mlab.dbpassword
+  },
+  postmark: require('./config.js').postmark
+};
