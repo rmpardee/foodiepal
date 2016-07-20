3f5899320046f28a50f9b63ca998c5f6e477b885
Andrew Tsao <andrew.tsao8@gmail.com>
2016-07-20 14:08:16 -0700

    (dev) Restores deployment config variables

--- server/config/env.js
+++ server/config/env.js
@@ -1,19 +1,17 @@
-var config = require('./config.js');
-
 // DEPLOYED version:
-// module.exports = {
-//   jwtSecret: process.env.JWT_SECRET,
-//   mlab: {
-//     dbuser: process.env.MLAB_DBUSER,
-//     dbpassword: process.env.MLAB_DBPASSWORD
-//   }
-// };
-
-// LOCAL version:
 module.exports = {
-  jwtSecret: config.expJwt.scrt,
+  jwtSecret: process.env.JWT_SECRET,
   mlab: {
-    dbuser: config.mlab.dbuser,
-    dbpassword: config.mlab.dbpassword
+    dbuser: process.env.MLAB_DBUSER,
+    dbpassword: process.env.MLAB_DBPASSWORD
   }
 };
+
+// LOCAL version:
+// module.exports = {
+//   jwtSecret: require('./config.js').expJwt.scrt,
+//   mlab: {
+//     dbuser: require('./config.js').mlab.dbuser,
+//     dbpassword: require('./config.js').mlab.dbpassword
+//   }
+// };
